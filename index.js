affb02590bfb2f7beeb8167da7194f8337ce4f98
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-05-26 00:58:48 +0600

    store user info api added and jwt stablished

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return req.status(401).send({ message: "Can't Authorize the Access" });
+    }
+    const accessToken = authHeader.split(" ")[1];
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
 
 const uri = `mongodb+srv://${process.env.DB_Admin}:${process.env.DB_Pass}@mna-sensors.4ipbl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,7 +34,14 @@ async function run() {
         await client.connect();
         const sensorsCollection = client.db('mnaSensors').collection('sensors');
         const orderCollection = client.db('mnaSensors').collection('orders');
+        const blogCollection = client.db('mnaSensors').collection('blogs');
+        const userCollection = client.db('mnaSensors').collection('users');
         
+        app.get('/blogs',async (req,res)=> {
+            const blogs = await blogCollection.find({}).toArray();
+            res.send(blogs);
+        })
+
         app.get('/sensors',async (req,res)=> {
             const query = {};
             const sensors = await sensorsCollection.find(query).toArray();
@@ -85,6 +106,30 @@ async function run() {
             );
             res.send(result);
         });
+
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updatedDoc,
+                options
+            );
+            const accessToken = jwt.sign(filter, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "1d",
+            });
+            res.send({ result, accessToken });
+        });
+
+        app.get('/users/count',async (req,res)=> {
+            const userCount = await userCollection.find({}).toArray();
+            res.send({usersNumber : userCount.length})
+        } )
 
     } finally {
 
